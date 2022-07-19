e4501ecc5e470db613863fa7363879842dadec16
Stjepan Poljak <stjepan.poljak@gmail.com>
2022-07-19 22:31:51 +0100

    Switch to JuicyPixels.

--- Main.hs
+++ Main.hs
@@ -1,33 +1,64 @@
 import Graphics.X11 (openDisplay, defaultScreenOfDisplay, screenNumberOfScreen
                     , getImage, getPixel, destroyImage, rootWindow, xyPixmap
                     , widthOfScreen, heightOfScreen, Image, allPlanes_aux
-                    , closeDisplay)
+                    , closeDisplay, Display, Screen, Position, Dimension
+                    )
+
+import qualified Codec.Picture as P (PixelRGB8(..), Pixel8, Image (..)
+                                    , DynamicImage(..), writePixel
+                                    , writeDynamicBitmap
+                                    )
+import qualified Codec.Picture.Types as PT (newMutableImage, unsafeFreezeImage)
 
 import Data.Bits ((.&.), shiftR)
-import Data.Binary (Word8)
 
-import qualified Data.ByteString as BS (pack, ByteString, empty)
-import Codec.BMP (packRGBA32ToBMP, writeBMP)
+import Control.Monad.Primitive (PrimMonad)
+import Control.Monad (mapM_, liftM, void)
 
-import qualified Data.Sequence as S (Seq, (><), fromList, empty)
-import qualified Data.Foldable as F (toList)
+packPixel :: Image -> (Int, Int) -> P.PixelRGB8
+packPixel i (x, y) = let rawPixel = map (fromIntegral . ((flip (.&.)) 255))
+                                  $ [shiftR greenShift 8, greenShift, pixel]
+                     in P.PixelRGB8 (rawPixel !! 0)
+                                    (rawPixel !! 1)
+                                    (rawPixel !! 2)
 
-packPixel :: Image -> (Int, Int) -> S.Seq Word8
-packPixel image (x, y) = S.fromList
-                       . map (fromIntegral . ((flip (.&.)) 255))
-                       $ [shiftR greenShift 8, greenShift, pixel, 0]
-
-    where pixel = getPixel image (fromIntegral x) (fromIntegral y)
+    where pixel = getPixel i (fromIntegral x) (fromIntegral y)
           greenShift = shiftR pixel 8
 
-packImage :: Int -> Int -> Image -> BS.ByteString
-packImage w h i = BS.pack
-                . F.toList
-                . foldl (\acc (x, y) -> acc S.>< packPixel i (x, y))
-                        S.empty
-                $ [ (x, h - y) | y <- [0..h-1], x <- [0..w-1] ]
+packImage :: (PrimMonad m) => Int -> Int -> Image -> m P.DynamicImage
+packImage w h i = liftM P.ImageRGB8 $ PT.newMutableImage w h
+              >>= \img -> const (PT.unsafeFreezeImage img)
+                      =<< mapM_ (\(x, y) -> P.writePixel img x y
+                                          . packPixel i $ (x, y))
+                          [ (x, h - y - 1) | y <- [0..h-1], x <- [0..w-1] ]
 
 path = "/home/stjepan/test.bmp"
+
+takePartScreenshot :: Display -> Screen -> Position -> Position
+                   -> Dimension -> Dimension -> IO P.DynamicImage
+takePartScreenshot display screen x y w h = do
+
+            rootw   <- rootWindow display
+                     . screenNumberOfScreen
+                     $ screen
+
+            image   <- getImage display rootw
+                                (fromIntegral x) (fromIntegral y)
+                                (fromIntegral w) (fromIntegral h)
+                                (fromIntegral allPlanes_aux)
+                                xyPixmap
+
+            pimage  <- packImage (fromIntegral w) (fromIntegral h)
+                                 image
+
+            destroyImage image
+
+            return pimage
+
+takeScreenshot :: Display -> Screen -> IO P.DynamicImage
+takeScreenshot display screen = let w = widthOfScreen screen
+                                    h = heightOfScreen screen
+                                in takePartScreenshot display screen 0 0 w h
 
 main :: IO ()
 main = do
@@ -35,24 +66,9 @@ main = do
     display <- openDisplay ""
 
     let screen = defaultScreenOfDisplay display
-    let scrnum = screenNumberOfScreen screen
 
-    rootw <- rootWindow display scrnum
+    P.writeDynamicBitmap path =<< takeScreenshot display screen
 
-    let screenWidth = widthOfScreen screen
-    let screenHeight = heightOfScreen screen
-
-    image <- getImage display rootw 0 0 (fromIntegral screenWidth)
-                                        (fromIntegral screenHeight)
-                      (fromIntegral allPlanes_aux) xyPixmap
-
-    writeBMP path . packRGBA32ToBMP (fromIntegral screenWidth)
-                                    (fromIntegral screenHeight)
-                  . packImage (fromIntegral screenWidth)
-                              (fromIntegral screenHeight)
-                  $ image
-
-    destroyImage image
     closeDisplay display
 
     return ()
